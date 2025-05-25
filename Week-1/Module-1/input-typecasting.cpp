afe4c68b57323f7dfbf1f8a0ccce225fb8f81da1
RJannat <ramimk097@gmail.com>
2025-05-25 16:39:37 +0600

    input in cpp

---
+++ Week-1/Module-1/input-typecasting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    char c;
+    cin >> x >> c;
+    cout << "this is x: " << x << endl << "and this is c: " << c;
+    return 0;
+}
