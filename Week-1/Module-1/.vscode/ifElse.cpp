f1eb1fb7e5206f52f82e43067192f2331bdf70a4
RJannat <ramimk097@gmail.com>
2025-05-25 18:09:27 +0600

    if else

---
+++ Week-1/Module-1/.vscode/ifElse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 12;
+    if (x % 2 == 0)
+    {
+        cout << "even" << endl;
+    }
+    else
+    {
+        cout << "odd" << endl;
+    }
+}
