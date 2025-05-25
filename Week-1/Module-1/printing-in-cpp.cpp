cd62d2133b4e93a25c7cd1f9eef5418f00e70101
RJannat <ramimk097@gmail.com>
2025-05-25 14:37:10 +0600

    printing in cpp

--- Week-1/Module-1/printing-in-cpp.cpp
+++ Week-1/Module-1/printing-in-cpp.cpp
@@ -1,1 +1,11 @@
 #include <iostream>
+int main()
+{
+    int a = 100;
+    char b = 'A';
+    double c = 4.44;
+
+    std::cout << "Hello World!" << std::endl;
+    std::cout << a << " " << b << " " << c;
+    return 0;
+}
