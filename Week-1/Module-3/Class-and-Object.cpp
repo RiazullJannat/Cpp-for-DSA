76d27af7f233d5d4ced189a78287b05e04dd69a0
RJannat <ramimk097@gmail.com>
2025-05-28 13:20:11 +0600

    taking input from user

--- Week-1/Module-3/Class-and-Object.cpp
+++ Week-1/Module-3/Class-and-Object.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 class Student
 {
-    public:
+public:
     char name[100];
     int roll;
     char section;
 };
 int main()
 {
-    Student rj;
-    char temp[100] = "Rjannat";
-    strcpy(rj.name, temp);
-    rj.roll = 1;
-    rj.section = 'a';
-    cout << "Name: " << rj.name << "\nRoll: " << rj.roll << "\nSection: " << rj.section;
+    Student rj, r;
+    cin.getline(rj.name, 100);
+    cin >> rj.roll >> rj.section;
+    cin.ignore();
+    cin.getline(r.name, 100);
+    cin >> r.roll >> r.section;
+    cout << "Name: " << rj.name << "\nRoll: " << rj.roll << "\nSection: " << rj.section << endl;
+    cout << "Name: " << r.name << "\nRoll: " << r.roll << "\nSection: " << r.section << endl;
 }
